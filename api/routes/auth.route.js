9b59d12e63018404945e4785dd2745b56277c255
nthao0812 <nthao0812@gmail.com>
2023-11-12 12:20:34 +0700

    create sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,3 +4,6 @@ import { signup } from "../controllers/auth.controller ";
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
+
+export default router;
